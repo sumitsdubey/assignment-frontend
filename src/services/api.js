cd3bc747b7b616fde487675a82e88ed75ce1da62
Sumit Dubey <sumitstudyguide@gmail.com>
2025-09-04 01:33:54 +0530

    link update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = 'http://localhost:8080/api/v1'
+const API_URL = 'https://assignment-u5il.onrender.com/api/v1'
 
 const api = axios.create({
     baseURL: API_URL
